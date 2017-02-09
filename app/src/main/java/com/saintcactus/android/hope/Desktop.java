3c2e1a65fab33f439477aeedf8327e9bce378d23
Laszlo Kerrick <laszlokerrick@gmail.com>
2017-02-09 20:10:11 +0300

    Add setting button (test the gitub)

--- app/src/main/java/com/saintcactus/android/hope/Desktop.java
+++ app/src/main/java/com/saintcactus/android/hope/Desktop.java
@@ -1,7 +1,10 @@
 package com.saintcactus.android.hope;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.saintcactus.android.hope.R;
 
@@ -11,5 +14,15 @@ public class Desktop extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desktop);
+
+        final Button btnSetting = (Button) findViewById(R.id.setting_button);
+
+        btnSetting.setOnClickListener(new View.OnClickListener(){
+          public void onClick(View v) {
+              Intent intent = new Intent("android.intent.action.setting");
+              startActivity(intent);
+          }
+          }
+        );
     }
 }
